288573c63ec8a5662aa853fd296b2c0f1665ef2a
Andrin Kessler <andrin.kessler@ik.me>
2024-03-14 15:22:11 +0100

    add id to todo list

--- Uebungen/Testat/controller/todo-controller.js
+++ Uebungen/Testat/controller/todo-controller.js
@@ -1,10 +1,15 @@
 import { todoStore } from "../services/todo-store.js";
 
-const 
+// const titleInput = document.querySelector('#title-input')
 
 export class TodoController {
     todo(req, res) {
         res.render("todo");
+
+        if (req.query.id) {
+            const todo = todoStore.get(req.query.id);
+            console.log(todo);
+        }
 
         if (req.query.title || req.query.importance || req.query.duedate || req.query.finished || req.query.description) {
                 todoStore.add(req.query.title, parseInt(req.query.importance), req.query.duedate, Boolean(req.query.finished), req.query.description);
