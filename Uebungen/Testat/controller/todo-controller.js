9827a95109a808d0643b14634e90c29db43fac58
Andrin Kessler <andrin.kessler@ik.me>
2024-03-18 11:37:48 +0100

    async todo method

--- Uebungen/Testat/controller/todo-controller.js
+++ Uebungen/Testat/controller/todo-controller.js
@@ -1,18 +1,16 @@
 import { todoStore } from "../services/todo-store.js";
 
-// const titleInput = document.querySelector('#title-input')
-
 export class TodoController {
-    todo(req, res) {
+    async todo(req, res) {
         res.render("todo");
 
         if (req.query.id) {
             const todo = todoStore.get(req.query.id);
-            console.log(todo);
+            console.log(await todo);
         }
 
         if (req.query.title || req.query.importance || req.query.duedate || req.query.finished || req.query.description) {
-                todoStore.add(req.query.title, parseInt(req.query.importance), req.query.duedate, Boolean(req.query.finished), req.query.description);
+            todoStore.add(req.query.title, parseInt(req.query.importance), req.query.duedate, Boolean(req.query.finished), req.query.description);
         }
     };
 }
