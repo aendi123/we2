487abf7777062c8c5f2a2502e8aefa4bc43a3b62
Andrin Kessler <andrin.kessler@ik.me>
2024-04-14 16:36:44 +0200

    move todopost into todocontroller

--- Testat_WE2_Andrin_Kessler/controller/todo-controller.ts
+++ Testat_WE2_Andrin_Kessler/controller/todo-controller.ts
@@ -2,7 +2,6 @@ import { todoStore } from "../services/todo-store";
 
 export class TodoController {
     async todo(req: any, res: any) {
-
         // User clicks Edit button of existing todo
         if (req.query.id) {
             const todo = await todoStore.get(req.query.id);
@@ -13,6 +12,28 @@ export class TodoController {
         else {
             const todo = {};
             res.render("todo", {todoValues: todo, createOrUpdate : "Create"});
+        }
+    }
+
+    async todopost(req: any, res: any) {
+        // User clicks Update button after editing existing todo
+        if (req.body.id !== "" && req.body.title && req.body.importance && req.body.duedate && req.body.description) {
+            const todo = await todoStore.update(req.body.id, req.body.title, parseInt(req.body.importance), req.body.duedate, Boolean(req.body.finished), req.body.description);
+            if (req.body.action === "Update & Overview") {
+                res.redirect("/");
+            } else {
+                res.render("todo", {todoValues: todo, createOrUpdate : "Update"});
+            }
+        }
+
+        // User clicks Create button after filling out form
+        else if (req.body.id === "" && req.body.title && req.body.importance && req.body.duedate && req.body.description) {
+            const todo = await todoStore.add(req.body.title, parseInt(req.body.importance), req.body.duedate, Boolean(req.body.finished), req.body.description);
+            if (req.body.action === "Create & Overview") {
+                res.redirect("/");
+            } else {
+                res.render("todo", {todoValues: todo, createOrUpdate : "Update"});
+            }
         }
     }
 }
--- Testat_WE2_Andrin_Kessler/routes/index-routes.ts
+++ Testat_WE2_Andrin_Kessler/routes/index-routes.ts
@@ -3,10 +3,9 @@ import express from 'express';
 const router = express.Router();
 import { indexController } from '../controller/index-controller';
 import { todoController } from '../controller/todo-controller';
-import { todoPostController } from '../controller/todo-post-controller';
 
 router.get("/", indexController.index.bind(indexController));
 router.get("/todo", todoController.todo.bind(todoController));
-router.post("/todo", todoPostController.todo.bind(todoPostController));
+router.post("/todo", todoController.todopost.bind(todoController));
 
 export const indexRoutes = router;
